b02fe362ee53881de02168952f8749bf57a9bc38
Adam Shannon <adam.shannon@banno.com>
2014-09-17 10:38:22 -0500

    fixing erlang cookie write script.

--- scripts/write-erlang-cookie.sh
+++ scripts/write-erlang-cookie.sh
@@ -2,13 +2,14 @@
 
 if [[ -z $ERLANG_COOKIE ]]; then
     exit 1;
-if;
+fi;
 
 echo "$ERLANG_COOKIE" > /.erlang.cookie
 
 # Setup the other default erlang cookie location.
+rm /var/lib/rabbitmq/.erlang.cookie
 ln -s /.erlang.cookie /var/lib/rabbitmq/.erlang.cookie
 chown rabbitmq /.erlang.cookie
 chmod 400 /.erlang.cookie
 
-exit 0;
+exit 0
